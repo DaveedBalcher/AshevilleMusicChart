43ecd26e3de90cb9d30aa4102102f5a664efda70
David Balcher <David.Balcher@gmail.com>
2024-11-11 10:53:35 -0500

    init

---
+++ data.js
@@ -0,0 +1,102 @@
+const data = [
+    {
+        name: "Secret Shame",
+        high_level_genre: "Rock/Alternative",
+        specific_genre: "Post-Punk",
+        weekly_spotify_listens: 4500,
+        weekly_soundcloud_listens: 1200,
+        instagram_follows: 6800,
+        image_url: "https://f4.bcbits.com/img/0017911853_0",
+        isNew: true
+    },
+    {
+        name: "Cave Grave",
+        high_level_genre: "Rock/Alternative",
+        specific_genre: "Garage Rock",
+        weekly_spotify_listens: 3500,
+        weekly_soundcloud_listens: 800,
+        instagram_follows: 4500,
+        image_url: "https://scontent-atl3-2.xx.fbcdn.net/v/t1.6435-9/75266170_105687757556798_5305740406904848384_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=cc71e4&_nc_ohc=F5kndXomcQkQ7kNvgHTR-td&_nc_zt=23&_nc_ht=scontent-atl3-2.xx&_nc_gid=A-CP6JF9PHRT8QohmtzGist&oh=00_AYCg7xJ7M1wEugC9zpDJja13uEJA8V2WeJxqRD4mO9axwQ&oe=6744DA5E",
+        isNew: false
+    },
+    {
+        name: "Wednesday",
+        high_level_genre: "Rock/Alternative",
+        specific_genre: "Indie Rock",
+        weekly_spotify_listens: 10000,
+        weekly_soundcloud_listens: 1500,
+        instagram_follows: 12000,
+        image_url: "https://media.pitchfork.com/photos/6524aed07792e31cf9313be1/4:3/w_3000,h_2250,c_limit/wednesday.jpg",
+        isNew: true
+    },
+    {
+        name: "Jon Stickley Trio",
+        high_level_genre: "Roots/Americana",
+        specific_genre: "Progressive Bluegrass",
+        weekly_spotify_listens: 7500,
+        weekly_soundcloud_listens: 600,
+        instagram_follows: 10000,
+        image_url: "https://worldmusiccentral.org/wp-content/uploads/Jon_Stickley_Trio.jpg",
+        isNew: false
+    },
+    {
+        name: "Town Mountain",
+        high_level_genre: "Roots/Americana",
+        specific_genre: "Traditional Bluegrass",
+        weekly_spotify_listens: 8200,
+        weekly_soundcloud_listens: 900,
+        instagram_follows: 9500,
+        image_url: "https://pinecastlemusic.com/wp-content/uploads/2018/06/Pinecastle-Artist-GALLERY-TownMountain-1.jpg",
+        isNew: false
+    },
+    {
+        name: "Empire Strikes Brass",
+        high_level_genre: "World/Fusion",
+        specific_genre: "Brass-Funk-Rock",
+        weekly_spotify_listens: 5600,
+        weekly_soundcloud_listens: 1100,
+        instagram_follows: 7000,
+        image_url: "https://mountainmusicfestwv.com/wp-content/uploads/2019/12/Empire-Strikes-Brass-lineup-page.jpg",
+        isNew: false
+    },
+    {
+        name: "The Get Right Band",
+        high_level_genre: "Rock/Alternative",
+        specific_genre: "Funk Rock",
+        weekly_spotify_listens: 4800,
+        weekly_soundcloud_listens: 750,
+        instagram_follows: 6000,
+        image_url: "https://highlandbrewing.com/wp-content/uploads/156894591_263324378693104_7461022895879682179_n.jpeg",
+        isNew: false
+    },
+    {
+        name: "Spaceman Jones & The Motherships",
+        high_level_genre: "Urban/Electronic",
+        specific_genre: "Alternative Hip-Hop",
+        weekly_spotify_listens: 5100,
+        weekly_soundcloud_listens: 1900,
+        instagram_follows: 5200,
+        image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTuD5bZfXOIGvP-TVHPhh0ejpiIqz89PYhA1Q&s",
+        isNew: false
+    },
+    {
+        name: "Jeff Santiago Y Los Gatos",
+        high_level_genre: "World/Fusion",
+        specific_genre: "Latin Rock",
+        weekly_spotify_listens: 4300,
+        weekly_soundcloud_listens: 1300,
+        instagram_follows: 5800,
+        image_url: "https://i.ytimg.com/vi/7PxR8jUNkHw/maxresdefault.jpg",
+        isNew: false
+    },
+    {
+        name: "Bask",
+        high_level_genre: "Rock/Alternative",
+        specific_genre: "Psychedelic Rock",
+        weekly_spotify_listens: 6400,
+        weekly_soundcloud_listens: 900,
+        instagram_follows: 7200,
+        image_url: "https://scontent-atl3-1.xx.fbcdn.net/v/t39.30808-6/342203983_1473632603040611_2156099143368689535_n.png?_nc_cat=108&ccb=1-7&_nc_sid=cc71e4&_nc_ohc=c5ZvHwPBYeMQ7kNvgEu0pdK&_nc_zt=23&_nc_ht=scontent-atl3-1.xx&_nc_gid=Au-L_H4b9NbdTBZcg8hW6xX&oh=00_AYBtNxVaY03LW9gxD3AXnJNryVi_YMhQnZFJzqUeurUhfg&oe=672F220A",
+        isNew: false
+    }
+];
