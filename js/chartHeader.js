119a2daddaae27b58dcc51e8f3631d80206e7611
David Balcher <David.Balcher@gmail.com>
2025-01-11 20:10:57 -0500

    update chart header text

--- js/chartHeader.js
+++ js/chartHeader.js
@@ -5,7 +5,7 @@ export function renderChartHeader(container, startDateStr, endDateStr) {
       <h2 class="chart-title">Top Asheville Artists</h2>
       <p class="chart-date-range">
         Ranked by <strong>Spotify Listens</strong> <br>
-        (${startDateStr} to ${endDateStr}) <br>
+        - ${startDateStr} to ${endDateStr} - <br>
       </p>
     </div>
   `;
