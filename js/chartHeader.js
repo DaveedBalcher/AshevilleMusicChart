0327d70ef04d9db534f64422925bb6a121088dbe
David Balcher <David.Balcher@gmail.com>
2025-01-11 23:35:22 -0500

    add pop up

--- js/chartHeader.js
+++ js/chartHeader.js
@@ -1,12 +1,25 @@
-// chartHeader.js (ES module version)
 export function renderChartHeader(container, startDateStr, endDateStr) {
   const headerHTML = `
     <div class="chart-header">
       <h2 class="chart-title">Top Asheville Artists</h2>
-      <p class="chart-date-range">
-        Ranked by <strong>Spotify Listens</strong> <br>
-        - ${startDateStr} to ${endDateStr} - <br>
-      </p>
+      <div class="chart-date-range">
+        <div class="content-wrapper">
+          <span class="ranking-text">
+            Ranked by <strong>Spotify Listens</strong>
+            <span class="icon-wrapper">
+              <i class="fas fa-info-circle info-icon"></i>
+              <span class="info-popup">
+                Rankings are based on global Spotify streams for artists based in the Asheville area. Updates weekly. <br>
+                <br>For each artists, the number of Spotify listens is shown for the most recent week and the difference from the previous week.<br>
+                <br><strong>🔥</strong> Indicates the artist with the highest increase in listens from the previous week. <br>
+              </span>
+            </span>
+          </span>
+        </div>
+        <div class="date-text">
+          ${startDateStr} to ${endDateStr}
+        </div>
+      </div>
     </div>
   `;
   container.innerHTML = headerHTML;
